5fc2baad4f7b1fa7df79da89b7ed828fca5891d7
Joseph Awolola <josephawolola16@gmail.com>
2026-09-02 16:38:02 +0100

    Halfway 08 streak

---
+++ 08-c-streak.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+
+    char date[] = "June 14", *newDate = date;
+    const char *a = "Awesome";
+
+    // printf("%s", newDate);
+    char x[10];
+    printf("Please, enter in your string: ");
+    // scanf("%s", x);
+    char text[30];
+    gets(text);
+    puts(text);
+
+    
+
+    return 0;
+}
+
+int read_line(char *str, int n)
+{
+    // the hard part
+    int c, i = 0;
+    
+    while ((c = getchar()) != '\0')
+    {
+        
+    }
+
+}
