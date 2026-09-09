e302aca9bd9ce91270621a80629ec972cf34ee7e
Joseph Awolola <josephawolola16@gmail.com>
2026-09-09 11:04:04 +0100

    15 c streak

--- 15-c-streak.c
+++ 15-c-streak.c
@@ -1,7 +1,29 @@
+#include <stdbool.h>
 #include <stdio.h>
 
+#define MAX(x, y) ((x)>(y)?(x):(y)) // function macros, we can use them for complex purposes (often called parameterized macros)
+#define IS_EVEN(n) ((n)%2==0)
+
+#define DISK_CAPACITY (5 * \
+    9*\
+    2*\
+    23*\
+    2)
 int main(int argc, char *argv[])
 {
-    
+    /*
+        Using command: 
+        gcc myprogram.c -o myprogram
+        ./myprogram -l remind.c
+    */
+
+    int w = MAX(3, 7);
+    int y = IS_EVEN(5); // false
+    printf("%d\n", w);
+    printf("%d\n", y);
+    for (int i = 0; i < argc; i++)
+    {
+        printf("%s\n", argv[i]);
+    }
     return 0;
 }
